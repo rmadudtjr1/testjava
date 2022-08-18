d0e19b21e3fa6a53cd235359648029b0ac0a3a57
it-kys <it-kys@hbi>
2022-08-18 10:27:40 +0900

    test

---
+++ src/javaFx/Main.java
@@ -0,0 +1,17 @@
+package javaFx;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Main extends Application{
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		// TODO Auto-generated method stub
+		primaryStage.show();
+	}
+
+	public static void main(String[] args) {
+		launch();
+	}
+}
